617319ed7fe959f23847e05f61c200fa3cf5b3a4
Nur Eren <92860209+erennur@users.noreply.github.com>
2021-12-12 22:33:34 +0300

    Create ödev_4.sql

---
+++ ödev_4.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT replacement_cost FROM film;
+SELECT COUNT (DISTINCT replacement_cost) FROM film;
+SELECT COUNT (DISTINCT title LIKE ('T&')) FROM film; SELECT title FROM film WHERE rating = 'G';
+SELECT COUNT (country LIKE ('-----')) FROM country;
+SELECT COUNT (city ILIKE 'R') FROM city;
